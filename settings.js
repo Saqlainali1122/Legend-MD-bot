516b84c57623d7af19e4582051dfecbb20a7fadb
Saqlainali1122 <saqlainali9200@gmail.com>
2025-11-09 05:32:10 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,28 @@
+// 🌟 Bot Configuration File 🌟
+// Edit these values to personalize your Legend boy
+
+const settings = {
+  // 🏷️ Sticker Settings
+  packname: '💎 LEGEND MD 💎',
+  author: '👑 LEGEND MD Official ',
+
+  // 🤖 Bot Identity
+  botName: '⚡ LEGEND BOY MD ⚡',
+  botOwner: '🌍 LEGEND BOYOfficial ',   // Change to your name if needed
+  ownerNumber: '923244180461',        // Your number without + symbol
+
+  // 🎬 APIs
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // For GIF commands
+
+  // ⚙️ Bot Mode
+  commandMode: 'public', // Options: 'public' (everyone) | 'private' (owner only)
+
+  // 📝 Meta Information
+  description: '💬 A fun, powerful & stylish WhatsApp MD bot created with ❤️ by LEGEND BOY POWER.',
+  version: '2.0.0',
+updateZipUrl: "https://github.com/Arslan-MD/Arslan-Tech-MD/archive/refs/heads/main.zip",
+  
+};
+
+// Export so other files can use it
+module.exports = settings;
